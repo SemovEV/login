c5d0ad3ec382ab653fd36756c941a52043bce7ed
SemovEV <67538619+SemovEV@users.noreply.github.com>
2022-05-27 12:18:26 +0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+const but = document.querySelector('.login_enter');
+const inputs = document.querySelectorAll('.input');
+const alert = document.querySelector('.alert');
+
+but.addEventListener('click', () => {    
+    for(var i = 0; i < users.length; i++){
+        if(inputs[0].value == users[i].login && inputs[1].value == users[i].password){
+            window.location.href = './../index.html';
+        }else{
+            alert.style.opacity = '1';
+        }
+    }
+})
---
+++ users.js
@@ -0,0 +1,10 @@
+let users = [
+    {
+        login : 'admin',
+        password : 'admin'
+    },
+    {
+        login : 'Егор',
+        password : 'qw3er5ty'
+    }
+]
